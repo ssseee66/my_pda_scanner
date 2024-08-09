0ae76b608dc85b0bb6d7d0fa157b8f8ec9de0c20
ssseee66 <feiyuanaichirou@outlook.com>
2024-08-09 18:30:02 +0800

    lsjs

--- android/src/main/java/com/example/my_pda_scanner/MyPdaScannerPlugin.java
+++ android/src/main/java/com/example/my_pda_scanner/MyPdaScannerPlugin.java
@@ -10,65 +10,67 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.EventChannel;
 
-/** MyPdaScannerPlugin */
+/**
+ * MyPdaScannerPlugin
+ */
 public class MyPdaScannerPlugin implements FlutterPlugin {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private Context applicationContext;
 
-  private static final String ACTION_DATA_CODE_RECEIVED =
-          "com.service.scanner.data";
-  private static final String DATA = "data";
+    private EventChannel eventChannel;
+    private Context applicationContext;
 
-  private static final String CHARGING_CHANNEL = "my_pda_channel";
+    private static final String ACTION_DATA_CODE_RECEIVED =
+            "com.service.scanner.data";
+    private static final String DATA = "data";
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), CHARGING_CHANNEL);
-    eventChannel.setStreamHandler(new EventChannel.StreamHandler() {
+    private static final String CHARGING_CHANNEL = "my_pda_channel";
 
-      private BroadcastReceiver chargingStateChangeReceiver;
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
 
-      @Override
-      public void onListen(Object arguments, EventChannel.EventSink events) {
-        chargingStateChangeReceiver = createChargingStateChangeReceiver(events);
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(ACTION_DATA_CODE_RECEIVED);
-        applicationContext.registerReceiver(
-                chargingStateChangeReceiver, filter);
-      }
+        eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), CHARGING_CHANNEL);
+        eventChannel.setStreamHandler(new EventChannel.StreamHandler() {
 
-      @Override
-      public void onCancel(Object arguments) {
-        applicationContext.unregisterReceiver(chargingStateChangeReceiver);
-        chargingStateChangeReceiver = null;
-      }
-    });
+            private BroadcastReceiver chargingStateChangeReceiver;
+
+            @Override
+            public void onListen(Object arguments, EventChannel.EventSink events) {
+                chargingStateChangeReceiver = createChargingStateChangeReceiver(events);
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(ACTION_DATA_CODE_RECEIVED);
+                applicationContext.registerReceiver(
+                        chargingStateChangeReceiver, filter);
+            }
+
+            @Override
+            public void onCancel(Object arguments) {
+                applicationContext.unregisterReceiver(chargingStateChangeReceiver);
+                chargingStateChangeReceiver = null;
+            }
+        });
 
 
-    applicationContext = flutterPluginBinding.getApplicationContext();
-  }
+        applicationContext = flutterPluginBinding.getApplicationContext();
+    }
 
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
 
-  private BroadcastReceiver createChargingStateChangeReceiver(final EventChannel.EventSink events) {
-    return new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        String code = intent.getStringExtra("ScanCode");
-        if (code != null) {
-          events.success(code);
-        }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        eventChannel.setStreamHandler(null);
+    }
 
-      }
-    };
-  }
+
+    private BroadcastReceiver createChargingStateChangeReceiver(final EventChannel.EventSink events) {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String code = intent.getStringExtra("ScanCode");
+                if (code != null) {
+                    events.success(code);
+                }
+
+            }
+        };
+    }
 
 }
