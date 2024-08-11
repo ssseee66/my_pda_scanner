fa81fafc513f791200d2d83d7b871adb79e864b9
ssseee66 <feiyuanaichirou@outlook.com>
2024-08-12 02:24:09 +0800

    kdksl

--- android/src/main/java/com/example/my_pda_scanner/MyPdaScannerPlugin.java
+++ android/src/main/java/com/example/my_pda_scanner/MyPdaScannerPlugin.java
@@ -11,6 +11,8 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.Result;
 
 /**
  * MyPdaScannerPlugin
